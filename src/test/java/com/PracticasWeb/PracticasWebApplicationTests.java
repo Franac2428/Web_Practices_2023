ac447e2d850fb778d94b02077bde5bddac7d2e17
HP PROBOOK <HP PROBOOK@DESKTOP-IGJKM84>
2023-02-02 20:20:56 -0600

    Practicas Web

---
+++ src/test/java/com/PracticasWeb/PracticasWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PracticasWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticasWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
